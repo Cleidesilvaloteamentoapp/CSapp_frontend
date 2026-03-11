ba5425fd78f7cb77cb7329fad0b9dd947dd514d9
onovotrafego <nacs.promoter@gmail.com>
2026-03-11 17:51:12 -0300

    chore: exclude PWA files from middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -74,6 +74,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|api).*)",
+    "/((?!_next/static|_next/image|favicon.ico|icons|manifest.json|sw.js|workbox|api).*)",
   ],
 };
